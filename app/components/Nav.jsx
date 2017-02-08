c0a9af51acdd8c762bd52546c44ad333edfe3343
Douglas Carlos <douglas.carlos@rh3software.local>
2017-02-08 10:03:27 -0300

    Initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,26 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = React.createClass({
+	render: function(){
+		return(
+			<div className="top-bar">
+				<div className="top-bar-left">
+					<ul className="menu">
+						<li className="menu-text">React Time App</li>
+						<li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight : 'bold'}} >Time</IndexLink></li>
+						<li><Link to="/countdown" activeClassName="active" activeStyle={{fontWeight : 'bold'}}>Countdown</Link></li>
+					</ul>
+				</div>
+				<div className="top-bar-right">
+					<ul className="menu">
+						<li className="menu-text">Creat by <a target="_blank" href="https://github.com/douglascbarbosa"> Douglas Carlos</a></li>
+					</ul>
+				</div>
+			</div>
+
+			)
+	}
+});
+	
+module.exports = Nav;
