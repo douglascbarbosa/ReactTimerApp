594362201e8322ddbc72d4fcc4ebef0fe9283755
Douglas Carlos <douglas.carlos@rh3software.local>
2017-02-22 09:40:27 -0300

    Created timer component

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -8,7 +8,7 @@ var Nav = React.createClass({
 				<div className="top-bar-left">
 					<ul className="menu">
 						<li className="menu-text">React Timer App</li>
-						<li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight : 'bold'}} >Time</IndexLink></li>
+						<li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight : 'bold'}} >Timer</IndexLink></li>
 						<li><Link to="/countdown" activeClassName="active" activeStyle={{fontWeight : 'bold'}}>Countdown</Link></li>
 					</ul>
 				</div>
--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,12 +1,59 @@
 var React = require('react');
+var Clock = require('Clock');
+var Controls = require('Controls');
 
 var Timer = React.createClass({
+	getInitialState: function(){
+		return {
+			count : 0,
+			timerStatus: 'paused'
+		}
+	},
+	componentDidUpdate: function (prevProps, prevState){
+		if (this.state.timerStatus !== prevState.timerStatus){
 
+			switch (this.state.timerStatus){
+				case 'started' : 
+					this.startTimer();
+					break;
+				case 'stopped' :
+					this.setState({
+						count: 0, 
+						timerStatus: 'paused'
+					});
+				case 'paused':
+					clearInterval(this.timer)
+					this.timer = undefined;
+					break;
+
+			}
+		}
+	},
+	componentWillUnmount : function (){
+		clearInterval(this.timer);
+	},
+	handleStatusChange: function (newStatus){
+		this.setState({timerStatus : newStatus});
+	},
+	startTimer: function () {
+
+		this.timer = setInterval(() => {
+			var newCount = this.state.count +1;
+			this.setState({
+				count: newCount
+			});
+
+		}, 1000);
+
+	},
 	render: function(){
+		var {count, timerStatus} = this.state;
 		return(
 
 			<div>
-				<h2>Timer teste</h2>
+				<h1 className="page-title">Timer</h1>
+				<Clock totalSeconds={count}/>
+				<Controls countdownStatus={timerStatus} onStatusChange={this.handleStatusChange} />
 			</div>
 
 		)
